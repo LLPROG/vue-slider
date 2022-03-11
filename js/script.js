a68170e6d963df66ed9f7deb0efc860cc16dc1a4
LLPROG <94935580+LLPROG@users.noreply.github.com>
2022-03-11 14:51:35 +0000

    add structure

---
+++ js/script.js
@@ -0,0 +1,36 @@
+const app = new Vue({
+	el: '#root', //container dove Vue agisce
+	data: {
+        indexCards: 0,
+		arrSlides: [
+			{
+				title: 'Svezia',
+				img:'01.jpg',
+				text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+			},
+            {
+				title: 'Svizzera',
+				img:'02.jpg',
+				text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+			},
+            {
+				title: 'Gran Bretagna',
+				img:'03.jpg',
+				text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+			},
+            {
+				title: 'Germania',
+				img:'04.jpg',
+				text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+			},
+            {
+				title: 'Paradise',
+				img:'05.jpg',
+				text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+			},
+		]
+	}
+});
+
+
+
